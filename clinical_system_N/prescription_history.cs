7d2c7ffd7f047aa388dd75539bbc44742ac8422f
OMXpress <oma.mohamed@nu.edu.eg>
2022-01-18 19:41:29 +0200

    2 user controls

---
+++ clinical_system_N/prescription_history.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clinical_system_N
+{
+    public partial class prescription_history : UserControl
+    {
+        public prescription_history()
+        {
+            InitializeComponent();
+        }
+
+        private void date_label(object sender, EventArgs e)
+        {
+
+        }
+
+        private void med_no_label(object sender, EventArgs e)
+        {
+
+        }
+
+        private void doc_name_lable(object sender, EventArgs e)
+        {
+
+        }
+
+        private void open_btn(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
