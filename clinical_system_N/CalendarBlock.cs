fdf9cef2b791137968ff92d6871dee36bb1a7204
O-Okasha <deviancesplit@gmail.com>
2022-01-18 17:59:44 +0200

    Partial calendar implementation

---
+++ clinical_system_N/CalendarBlock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clinical_system_N
+{
+    public partial class CalendarBlock : UserControl
+    {
+        public CalendarBlock()
+        {
+            InitializeComponent();
+        }
+
+        private void CalendarBlock_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
